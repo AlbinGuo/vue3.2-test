1313d30a8d736ba8069ac03e19a0f8a72a7a7001
guonan <838093891@qq.com>
2022-11-04 00:23:52 +0800

    feat: prettierrc

---
+++ src/components/compose/directive.vue
@@ -0,0 +1,8 @@
+<template>
+  <a-card title="指令Directive">
+    搜素
+  </a-card>
+</template>
+
+<script lang="ts" setup>
+</script>
--- src/components/compose/index.vue
+++ src/components/compose/index.vue
@@ -1,34 +1,33 @@
 <template>
-  <div>
-    <vbind v-bind="objectOfAttrs"></vbind>
-  </div>
-  <div>
-    <counter></counter>
-  </div>
-  <div>
-    <expression></expression>
-  </div>
+    <div>
+        <vbind v-bind="objectOfAttrs"></vbind>
+    </div>
+    <div>
+        <counter></counter>
+    </div>
+    <div>
+        <expression></expression>
+    </div>
+    <div>
+        <directive></directive>
+    </div>
 </template>
 
 <script lang="ts" setup>
 import counter from './counter.vue';
 import vbind from './vbind.vue';
-import expression from './expression.vue'
+import expression from './expression.vue';
+import directive from './directive.vue';
 
 interface Attribute {
-  id: string;
-  clazz: string;
+    id: string;
+    clazz: string;
 }
 
-const objectOfAttrs = ref<Attribute>(
-  {
+const objectOfAttrs = ref<Attribute>({
     id: 'container-id',
-    clazz: 'wrapper-clazz',    
-  }
-);
-
+    clazz: 'wrapper-clazz'
+});
 </script>
 
-<style lang='scss' scoped>
-
-</style>
+<style lang="scss" scoped></style>
