86dc1bacb0c144c9e58702de789756b9ad8a68cc
guonan <guonan@handaotech.com>
2022-11-10 00:20:05 +0800

    add 深度监听

--- src/components/study/WatchEffect.vue
+++ src/components/study/WatchEffect.vue
@@ -3,6 +3,11 @@
 </template>
 
 <script lang='ts' setup>
+/**
+ * watchEffect，它立即执行传入的一个函数，同时响应式追踪其依赖，并在其依赖变更时重新运行该函数。
+ * 换句话说：watchEffect相当于将watch 的依赖源和回调函数合并，当任何你有用到的响应式依赖更新时，
+ * 该回调函数便会重新执行。不同于 watch，watchEffect 的回调函数会被立即执行（即 { immediate: true }）
+ */
 </script>
 
 <style scoped>
--- src/components/study/watch.vue
+++ src/components/study/watch.vue
@@ -24,6 +24,10 @@
     <a-card title='监听props对象中某几个属性变化'>
         <p>props-age: {{ props.nianling }}</p>
     </a-card>
+
+    <a-card title='深度监听'>
+        <p>张三的工资: <input type='text' v-model='zhangsan.work.salary'></p>
+    </a-card>
 </template>
 
 <script lang='ts' setup>
@@ -97,7 +101,38 @@ watch(() => props.nianling,(newVal,oldVal)=>{
     console.log(newVal,oldVal)
 });
 
+// ================================ 深度监听
+const zhangsan = ref({
+    name: '张三',
+    age: 40,
+    work: {
+        salary: 3000,
+        year: 2
+    }
+});
 
+/**
+ * 开启深度监听deep:true
+ * ref定义对象类型使用watch监听对象时监听的对象外层可以使用2个方式
+ */
+watch(zhangsan.value, (newVal, oldValue) => {
+    console.log(newVal, oldValue);
+});
+
+watch(zhangsan, (newVal, oldValue) => {
+    console.log(newVal, oldValue);
+}, {deep: true});
+
+
+const x = ref(1)
+const y = ref(1)
+// getter 函数
+watch(
+    () => x.value + y.value,
+    (sum) => {
+        console.log(`sum of x + y is: ${sum}`)
+    }
+)
 
 </script>
 
